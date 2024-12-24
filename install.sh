6b3610a693b6f46e78229260c4b8d968c2e685da
StephenLangHall <stephenlh2007@gmail.com>
2024-12-24 15:07:26 -0500

    add start and stop commands

--- install.sh
+++ install.sh
@@ -3,5 +3,7 @@
 case ${1} in
 	"r"|"remove") sudo rm /etc/systemd/system/downloadsort.service /usr/bin/DownloadSort.sh ;;
 	"i"|"install") sudo cp downloadsort.service /etc/systemd/system/downloadsort.service; sudo cp DownloadSort.sh /usr/bin/DownloadSort.sh ;;
+	"s"|"start") sudo systemctl start downloadsort.service ;;
+	"k"|"kill") sudo systemctl stop downloadsort.service ;;
 	*) echo "install.sh [r/remove] to remove\ninstall.sh [i/install] to install" ;;
 esac
